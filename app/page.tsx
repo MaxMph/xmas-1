86dd659daff2d44f3ad11c7b330fe4c669e15834
MaxMph <hurleymax255@gmail.com>
2025-12-14 00:41:40 -0500

    font

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
       <main>
         <div className="flex flex-col items-center gap-6 text-center">
           <h1 className={"text-6xl self-center my-10"}>There are gifts that need wrapping!</h1>
-          <p className={"text-3xl font-bold"}>cash: {count}</p>
+          <p className={"text-3xl"}>cash: {count}</p>
           <ButtonClick onClick={() => {setCount(count + 1)}} />
         </div>
       </main>
