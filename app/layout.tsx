ea836c368d30a0a6c55f7fe690bb0ee0ec257f50
MaxMph <hurleymax255@gmail.com>
2025-12-14 00:19:09 -0500

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
 
-export default function RootLayout({children}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html>
       <body>
